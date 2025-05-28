4a2c21fb9ccff5a6714d93a62a02bab0705e48f4
Efe Kocasu <kocasuhasanefe@gmail.com>
2025-05-28 20:23:10 +0300

    Doc-string has been improved

--- 4pp_t1m3r.py
+++ 4pp_t1m3r.py
@@ -1,2 +1,12 @@
-#This file will be app timer backend file
-print("This file will be app timer backend file")
+"""
+This file will be app timer backend file
+
+Doc-string English part is writting right now,it will be replaced soon. For a now this part will be in Turkish.
+
+Uygulamaların ve web sitelerinin sürelerinin takip edilmesi ve hedef süre belirlenmesi yapılıcak. 
+Bu sayede belirlenen sürenin sonunda uygulama/web siteye cihazdan erişilemeyecek.
+Hangi uygulama ne kadar kullanılmış bunun istatistiki takibi yapılıcak ve kullanıcıya verilebilecek.
+Uygulamanın GUI'si şuanlık olmayacak ancak ilerleyen sürümlerde o da eklenicek.
+Arayüze şimdilik terminal ekranı üzerinden erişilicek.
+
+"""
